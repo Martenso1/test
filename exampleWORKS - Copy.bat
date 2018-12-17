0f2688fbd00bfcc1f676225a546132f58604a97f
Martenso1 <45899826+Martenso1@users.noreply.github.com>
2018-12-17 01:47:54 +0000

    Update exampleWORKS - Copy.bat

--- exampleWORKS - Copy.bat
+++ exampleWORKS - Copy.bat
@@ -6,17 +6,17 @@ ECHO ...............................................
 ECHO PRESS 1, 2 , 3 OR 4 to select your task, or 5 to EXIT.
 ECHO ...............................................
 ECHO.
-ECHO 1 - Open CPU
-ECHO 2 - Display RAM
-ECHO 3 - Show ALL
-ECHO 4 - DISK
+ECHO 1 - Show  CPU Information
+ECHO 2 - Display RAM Information
+ECHO 3 - Show Partition Information
+ECHO 4 - Show ALL
 ECHO 5 - EXIT
 ECHO.
-SET /P M=Type 1, 2, 3, or 4 then press ENTER:
+SET /P M=Type 1, 2, 3, 4 or 5 then press ENTER:
 IF %M%==1 GOTO CPU
 IF %M%==2 GOTO RAM
-IF %M%==3 GOTO ALL
-IF %M%==4 GOTO DISK
+IF %M%==3 GOTO DISK
+IF %M%==4 GOTO ALL
 IF %M%==5 GOTO EOF
 :ALL
 CLS
@@ -66,5 +66,9 @@ ECHO. CPU usage in %%:
 for /f "skip=1" %%p in ('wmic cpu get loadpercentage') do ECHO( %%p
 GOTO MENU
 :DISK
+CLS
+ECHO................................................
+ECHO(This is your Partition Information
+ECHO................................................
 WMIC LOGICALDISK GET SIZE,FREESPACE,CAPTION
 GOTO MENU
